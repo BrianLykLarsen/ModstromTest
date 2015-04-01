ebd14c48a57e44a6a8c18bae5d02a11cd5dc5b2b
Fabrice Loudet <flo@novicell.dk>
2015-04-01 13:55:58 +0200

    init

---
+++ packages/Novicell.Umbraco.Mvc.Library.1.0.2/content/Models/BaseModel.cs.pp
@@ -0,0 +1,11 @@
+﻿using Umbraco.Web;
+using Umbraco.Web.Models;
+
+namespace $rootnamespace$.Models
+{
+    public class BaseModel : RenderModel
+    {
+        public BaseModel() : base(UmbracoContext.Current.PublishedContentRequest.PublishedContent) { }       
+        public BaseModel(IPublishedContent content) : base(content) { }      
+    }
+}
