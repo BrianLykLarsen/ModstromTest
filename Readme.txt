53b0abda420bcde4ba2e24779f3a6e2d5ead795a
obr <obr@novicell.dk>
2015-05-06 07:20:03 +0200

    csproj

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+Setup IIS
+Build Project
+Republish Site in umbraco
+
+BAM! You are up and running :-)
---
+++ packages/Novicell.Umbraco.Mvc.Library.1.0.2/content/Events/Helpers/Global.cs.pp
@@ -0,0 +1,25 @@
+﻿using System.Web;
+
+
+namespace $rootnamespace$.Helpers
+{
+    // copied from the Hybrid framework + http://aspadvice.com/blogs/ssmith/archive/2007/10/29/VaryByCustom-Caching-By-User.aspx
+    public class Global : Umbraco.Web.UmbracoApplication
+    {
+        public override string GetVaryByCustomString(HttpContext context, string custom)
+        {
+            if (custom.ToLower() == "url")
+            {
+                return "url=" + context.Request.Url.AbsoluteUri;
+            }
+
+            if (custom.ToLower() == "url;isLogged")
+            {
+                string userName = context.User.Identity.Name;
+                return "url=" + context.Request.Url.AbsoluteUri + "&isLogged=" + userName;
+            }
+
+            return base.GetVaryByCustomString(context, custom);
+        }
+    }
+}
