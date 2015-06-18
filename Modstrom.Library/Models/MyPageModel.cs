ceea17905735ea29731a418646fd99042b943778
Michael S?lvsteen <msl@novicell.dk>
2015-06-18 12:50:28 +0200

    case 72276: MVC Basic template

---
+++ Modstrom.Library/Models/MyPageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modstrom.Library.Models
+{
+    public class MyPageModel : BaseModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
