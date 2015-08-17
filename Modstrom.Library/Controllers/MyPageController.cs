c17b7938287c566324016070373099b4718b1fcc
jha <jha@novicell.dk>
2015-08-17 07:24:22 +0200

    case 75137: Gulp setup

---
+++ Modstrom.Library/Controllers/MyPageController.cs
@@ -0,0 +1,20 @@
+﻿using Modstrom.Library.Controllers.Base;
+using Modstrom.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Modstrom.Library.Controllers
+{
+    public class MyPageController : BaseSurfaceController
+    {
+        public ActionResult MyPage()
+        {
+            var model = new MyPageModel();
+            return CurrentTemplate(model);
+        }
+    }
+}
