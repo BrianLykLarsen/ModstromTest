aa2955e7474a27e4ba184d2d43f338104e997a23
Emil Ankersen (EAN) <ean@novicell.dk>
2015-08-24 15:27:34 +0200

    Case 75338 - Menuen skal f?lge med, n?r man scroller ned af sitet.

--- Modstrom.Website/Scripts/master.js
+++ Modstrom.Website/Scripts/master.js
@@ -27,6 +27,7 @@ $(function () {
     modstrom.master.showMoreCases();
     modstrom.master.toggleTestimonial();
     modstrom.master.formFix();
+    modstrom.master.backToTop();
     modstrom.master.responsiveImage('default');
     modstrom.master.responsiveImage('mobile');
     modstrom.master.responsiveImage('tablet');
@@ -366,6 +367,32 @@ modstrom.master = modstrom.master || function () {
         }
     }
 
+    function backToTop() {
+        // browser window scroll (in pixels) after which the "scroll-top-top" link is shown
+        var offset = 700,
+            //duration of the top scrolling animation (in ms)
+            scroll_top_duration = 700,
+            //grab the "scroll-top-top" link
+            $back_to_top = $('.back-to-top');
+
+        //hide or show the "scroll-top-top" link
+        $(window).scroll(function () {
+            ($(this).scrollTop() > offset) ? $back_to_top.addClass('back-to-top-visible') : $back_to_top.removeClass('back-to-top-visible');
+        });
+
+        //smooth scroll to top
+        $back_to_top.on('click', function (event) {
+            event.preventDefault();
+            $('body,html').animate({
+                scrollTop: 0,
+            }, scroll_top_duration
+            );
+        });
+    }
+   
+
+
+
     return {
     	resetUI: resetUI,
     	mainNavigation: mainNavigation,
@@ -384,7 +411,8 @@ modstrom.master = modstrom.master || function () {
     	support: support,
     	showMoreCases: showMoreCases,
     	toggleTestimonial: toggleTestimonial,
-    	formFix:formFix
+    	formFix: formFix,
+        backToTop: backToTop
     }
 }();
 
