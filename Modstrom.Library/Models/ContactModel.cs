3f076454e47e22654695361e70bc59ae84f46110
jha <jha@novicell.dk>
2016-02-16 08:25:13 +0100

    case 87988: Added scrolltolink in burger menu

---
+++ Modstrom.Library/Models/ContactModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Umbraco.Core.Models;
+
+namespace Modstrom.Library.Models
+{
+    public class ContactModel : BaseModel
+    {
+        public ContactModel() { }
+        public ContactModel(IPublishedContent content) : base(content) { }  
+    
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [MinLength(20, ErrorMessage = "Your message is less than 20 characters. Please provide more details to your message.")]
+        public string Message { get; set; }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,9 @@
+Setup IIS
+Build Project
+Republish Site in umbraco
+
+BAM! You are up and running :-)
+
+NOTE! IMPORTANT!
+This project uses custom forms.
+DO NOT UPGRADE UMBRACO FORMS UNLESS YOU BACKUP forms.cshtml
